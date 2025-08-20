80cf8f570604d3d5ddfbc6d05fbbb4b8b9ae0fc1
adelayoub <adelayoub.maaziz@gmail.com>
2025-08-20 16:22:59 +0100

    feat: enhance builtin system with proper function pointers and improved exit command

--- src/builtins/built.c
+++ src/builtins/built.c
@@ -1,5 +1,9 @@
 #include "cshell.h"
 
+// Forward declarations
+int builtin_pwd_wrapper(char **args);
+int builtin_env_wrapper(char **args);
+
 static char *builtin_names[] = {
     "echo",
     "cd",
@@ -13,12 +17,24 @@ static char *builtin_names[] = {
 static int (*builtin_functions[])(char **) = {
     builtin_echo,
     builtin_cd,
-    NULL,  // pwd takes no args
-    NULL,  // env takes no args
+    builtin_pwd_wrapper,  // pwd wrapper
+    builtin_env_wrapper,  // env wrapper
     builtin_export,
     builtin_unset,
     builtin_exit
 };
+
+int builtin_pwd_wrapper(char **args)
+{
+    (void)args; // Suppress unused parameter warning
+    return (builtin_pwd());
+}
+
+int builtin_env_wrapper(char **args)
+{
+    (void)args; // Suppress unused parameter warning
+    return (builtin_env());
+}
 
 int is_builtin(char *command)
 {
@@ -49,12 +65,7 @@ int execute_builtin(char **args)
     {
         if (dl_strncmp(args[0], builtin_names[i], dl_strlen(builtin_names[i]) + 1) == 0)
         {
-            if (i == 2)  // pwd
-                return (builtin_pwd());
-            else if (i == 3)  // env
-                return (builtin_env());
-            else if (builtin_functions[i])
-                return (builtin_functions[i](args));
+            return (builtin_functions[i](args));
         }
         i++;
     }
@@ -64,22 +75,45 @@ int execute_builtin(char **args)
 int builtin_exit(char **args)
 {
     int exit_code;
+    int i;
 
     if (args[1])
     {
-        exit_code = dl_atoi(args[1]);
-        if (exit_code == 0 && args[1][0] != '0')
+        // Check if argument is numeric
+        i = 0;
+        if (args[1][0] == '+' || args[1][0] == '-')
+            i++;
+        
+        while (args[1][i])
         {
-            print_error("exit", "numeric argument required");
-            g_data.exit_status = 255;
-            return (255);
+            if (!dl_isdigit(args[1][i]))
+            {
+                print_error("exit", "numeric argument required");
+                g_data.last_exit_status = 255;
+                return (255);
+            }
+            i++;
         }
+        
+        exit_code = dl_atoi(args[1]);
+        
+        // Handle exit status range (0-255, wrap around)
+        if (exit_code < 0)
+            exit_code = 256 + (exit_code % 256);
+        else
+            exit_code = exit_code % 256;
+        
         g_data.exit_status = exit_code;
+        g_data.last_exit_status = exit_code;
     }
     else
     {
-        g_data.exit_status = EXIT_SUCCESS;
+        g_data.exit_status = g_data.last_exit_status;
     }
+    
+    // Print exit message if interactive
+    if (g_data.interactive_mode)
+        dl_putendl_fd("exit", STDOUT_FILENO);
     
     // Cleanup and exit
     cleanup_shell();
